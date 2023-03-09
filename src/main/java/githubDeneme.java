c5ae3e4ee300e9a5ee6d40068986a856e09dde58
techproed <burcupiaf@gmail.com>
2023-03-09 12:33:21 -0600

    deneme 9 m

--- src/main/java/githubDeneme.java
+++ src/main/java/githubDeneme.java
@@ -2,5 +2,18 @@ public class githubDeneme {
     public static void main(String[] args) {
         System.out.println("Deneme calisma");
         System.out.println("ali... ");
+
+
+
+
+
+
+
+
+        int a=103;
+        int b=102;
+        int c=101;
+        System.out.println(a+b+c);
+
     }
 }
